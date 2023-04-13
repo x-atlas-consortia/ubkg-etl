2a91af77dcfd566e31ec8e855e768e6ce6a29762
AlanSimmons <jas971@pitt.edu>
2023-04-13 08:20:59 -0500

    GenCode: loads and translates annotations and metadata (Entrez, RefSeq, SwissProt, and TrEMBL) and merges metadata with annotations to build a consolidated data source.

--- generation_framework/gencode/gencode.py
+++ generation_framework/gencode/gencode.py
@@ -49,12 +49,13 @@ def get_source_files(owl_dir: str, owlnets_dir: str)-> list[str]:
 
     return list_gtf
 
-def load_GTF_into_DataFrame(file_pattern:str, path: str, skip_lines:int=0) -> pd.DataFrame:
+def load_GTF_into_DataFrame(file_pattern:str, path: str, skip_lines: int=0, rows_to_read: int=0) -> pd.DataFrame:
 
     # Loads a GTF file into a Pandas DataFrame, showing a progress bar.
     # file_pattern: portion of a name of a GTF file--e.g., "annotation"
     # path: path to folder containing GTF files.
     # skip_lines: number of lines to skip
+    # rows_to_read: optional number of rows to read. In this case, the default means to read all rows.
 
     list_gtf = os.listdir(path)
 
@@ -66,10 +67,16 @@ def load_GTF_into_DataFrame(file_pattern:str, path: str, skip_lines:int=0) -> pd
             # Get number of lines in file.
             with open(gtffile, 'r') as fp:
                 lines = len(fp.readlines())
+
+            if rows_to_read == 0:
+                nrows = lines
+            else:
+                nrows = rows_to_read
+
             # Read file in chunks, updating progress bar after each chunk.
             listdf = []
             with tqdm(total=lines,desc='Loading file') as bar:
-                for chunk in pd.read_csv(gtffile,chunksize=1000,comment='#',sep='\t',nrows=100):
+                for chunk in pd.read_csv(gtffile,chunksize=1000,comment='#',sep='\t',nrows=nrows):
                     listdf.append(chunk)
                     bar.update(chunk.shape[0])
 
@@ -80,68 +87,32 @@ def load_GTF_into_DataFrame(file_pattern:str, path: str, skip_lines:int=0) -> pd
     ulog.print_and_logger_info(f'Error: missing file with name that includes \'{file_pattern}\'.')
     exit(1)
 
-def get_key_value_column(search_key: str, key_value_column: pd.Series) -> pd.Series:
+def build_key_value_column(dfGTFl1: pd.DataFrame, search_key: str) -> pd.DataFrame:
 
-    # Returns a consolidated value column from a key-value column in GTF format.
+    # Builds a consolidated value column from a key-value column in GTF format and adds it to the
+    # input DataFrame.
     # Refer to https://www.gencodegenes.org/pages/data_format.html for the key-value column.
 
     # Arguments:
+    # dfGTFl1- DataFrame of data downloaded from FTP, after the key-value column has been split by the
+    #          Level 1 delimiter (colon)
     # search_key - name of the key to extract from the key-value column.
     # key_value_column - the key-value column from the DataFrame.
 
-    # --------------------------
-    # The key-value column uses two levels of delimiting:
-    # Level 1 - delimiter between key/value pairs = ;
-    # Level 2 - delimiter between key and value = ' '
-
-    # Excerpt of a key-value column (from the general annotation GTF):
-    # "gene_id "ENSG00000223972.5"; transcript_id "ENST00000456328.2"; gene_type "transcribed_unprocessed_pseudogene"; gene_name "DDX11L1";"
-
-    # Key/value pairs do not have static locations--i.e., a key/value pair may be in column X in one row and column Y
-    # in another.
-    # Furthermore, some keys have multiple values in the same row--e.g., tag, ont.
-    # For example, row 11 of the annotation shows tag "basic" in column 11 and tag "Ensembl_canonical" in column 12.
-    #
-    # Build a series of values for a particular key by looking through all columns.
-
-    # Example (x, y, z, a are keys):
-    #     columns
-    # row       1         2         3       4
-    # 0         x 20
-    # 1         x 30
-    # 2                    x 40
-    # 3         a 99
-    # 4         y 25       z 30     x 50    x 60 <--note multiple values for x
-
-    # The desired result if search_key = x is a series of values corresponding to key=x, sorted in the original order
-    # of the argument series:
-    # 0 20
-    # 1 30
-    # 2 40
-    # 3
-    # 4 50,60
-
     # -----------------------------
-
-    # Level 1 split
-    # Split the key/value pairs using the colon delimiter.
-    dfSplit_level_1 = key_value_column.str.split(';', expand=True)
-    # Normalize empty column empty values to NaN.
-    dfSplit_level_1 = dfSplit_level_1.replace({'None': np.nan}).replace({None: np.nan}).replace({'': np.nan})
-    # Remove completely empty columns.
-    dfSplit_level_1 = dfSplit_level_1.dropna(axis=1, how='all')
-
-
     # Level 2 split
     # Split each column from the Level 1 split into key and value columns.
-
     listval = []
-    for col in dfSplit_level_1:
+
+    for col in dfGTFl1:
+        # The columns from the first-level split have names that are numbers starting with 0.
+        if not isinstance(col, int):
+            continue
 
         # Split each key/value pair column into separate key and value columns, using the space delimiter.
-        # The strip function removes leading spaces that can be mistaken for delimiters.
-        dfSplit_level_2 = dfSplit_level_1[col].str.strip()
-        dfSplit_level_2 = dfSplit_level_2.str.split(' ', expand=True)
+        # The strip function removes leading spaces that can be mistaken for delimiters, such as occurs in the
+        # first key-value column.
+        dfSplit_level_2 = dfGTFl1[col].str.strip().str.split(' ', expand=True)
         # The split gives the key and value columns numeric names. Rename for clarity.
         dfSplit_level_2.columns = ['key','value']
 
@@ -156,18 +127,44 @@ def get_key_value_column(search_key: str, key_value_column: pd.Series) -> pd.Ser
 
     if len(listval) > 0:
         # Build the entire list of values for the key.
-        dfReturn = pd.concat(listval)
+        dfValues = pd.concat(listval)
 
         # Concatenate multiple values that appear for the key in a row.
         # This collapses the result down to the correct number of rows.
-        dfReturn = dfReturn.reset_index(names='rows')
-        dfReturn = dfReturn.groupby('rows').agg({'value': lambda x: ','.join(x)})
-        sReturn = dfReturn['value']
+        dfValues = dfValues.reset_index(names='rows')
+        dfValues = dfValues.groupby('rows').agg({'value': lambda x: ','.join(x)})
+        sValues = dfValues['value']
     else:
         # Return an empty series.
-        sReturn = pd.Series(index=key_value_column.index.copy(), dtype='str')
+        sValues = pd.Series(index=dfGTFl1.index.copy(), dtype='str')
 
-    return sReturn
+    # Add the consolidated list of values for the search key to the input DataFrame.
+    dfGTFl1[search_key] = sValues
+
+    return
+
+def split_column9_level1(dfGTF: pd.DataFrame) ->pd.DataFrame:
+
+    # Perform the first-level split of the key-value column (9th) of a GTF file, adding
+    # separated key-value pairs as columns.
+
+    # Refer to in-line documentation in the function build_Annotation_DataFrame for details on the
+    # processing of the key-value column.
+
+    key_value_column = dfGTF['column_9']
+
+    # Split the key/value pairs using the colon delimiter.
+    dfSplit_level_1 = key_value_column.str.split(';', expand=True)
+    # Normalize empty column empty values to NaN.
+    dfSplit_level_1 = dfSplit_level_1.replace({'None': np.nan}).replace({None: np.nan}).replace({'': np.nan})
+    # Remove completely empty columns.
+    dfSplit_level_1 = dfSplit_level_1.dropna(axis=1, how='all')
+
+    # Add columns of separated key-value pairs to the input.
+
+    for col in dfSplit_level_1:
+        dfGTF[col] = dfSplit_level_1[col]
+    return dfGTF
 
 def build_Annotation_DataFrame(path: str) -> pd.DataFrame:
 
@@ -180,14 +177,15 @@ def build_Annotation_DataFrame(path: str) -> pd.DataFrame:
     # Because the GenCode version is part of the file name (e.g., gencode.v41.annotation.gtf),
     # search on a file pattern.
     # The first five rows of the annotation file are comments.
-    df = load_GTF_into_DataFrame(file_pattern="annotation", path=args.owlnets_dir, skip_lines=5)
+    print('DEBUG: reading < total rows from annotation file.')
+    dfGTF = load_GTF_into_DataFrame(file_pattern="annotation", path=args.owlnets_dir, skip_lines=5,rows_to_read=1000)
 
-    # Add column headers.
-    df.columns = ['chromosome_name', 'annotation_source', 'feature_type', 'genomic_start_location',
+    # The GTF file does not have column headers. Add these with values from the specification.
+    dfGTF.columns = ['chromosome_name', 'annotation_source', 'feature_type', 'genomic_start_location',
                               'genomic_end_location', 'score', 'genomic_strand', 'genomic_phase', 'column_9']
 
     # Add columns corresponding to the key/value pairs in the 9th column.
-    # GTF keys.
+    # GTF key names are from the specification.
     list_keys = [
         # required
         'gene_id',
@@ -219,11 +217,74 @@ def build_Annotation_DataFrame(path: str) -> pd.DataFrame:
         'mgi_id'
     ]
 
-    df['column_9'] = df['column_9'].str.replace('"', '')
-    for k in list_keys:
-        df[k] = get_key_value_column(k, df['column_9'])
+    # --------------------------
+    # The key-value column uses two levels of delimiting:
+    # Level 1 - delimiter between key/value pairs = ;
+    # Level 2 - delimiter between key and value = ' '
 
-    return df
+    # Excerpt of a key-value column (from the general annotation GTF):
+    # "gene_id "ENSG00000223972.5"; transcript_id "ENST00000456328.2"; gene_type "transcribed_unprocessed_pseudogene"; gene_name "DDX11L1";"
+
+    # Key/value pairs do not have static locations--i.e., a key/value pair may be in column X in one row and column Y
+    # in another.
+    # Furthermore, some keys have multiple values in the same row. For example, row 11 of the annotation shows tag "basic" in column 11
+    # and tag "Ensembl_canonical" in column 12.
+    #
+
+    # This means that the key-value columns after the Level 1 split will resemble the following
+    # (x, y, z, a are keys):
+    #     columns
+    # row       1         2         3       4
+    # 0         x 20
+    # 1         x 30
+    # 2                    x 40
+    # 3         a 99
+    # 4         y 25       z 30     x 50    x 60 <--note multiple values for the x key
+
+    # The desired result if search_key = x is a series of values corresponding to key=x,
+    # sorted in the original row order, with multiple values collected into lists--e.g.,
+    #  x
+    # 0 20
+    # 1 30
+    # 2 40
+    # 3
+    # 4 50,60
+
+    # Level 1 split
+    ulog.print_and_logger_info('Splitting the 9th column into individual key-value columns...')
+    dfGTF['column_9'] = dfGTF['column_9'].str.replace('"', '')
+    dfGTF = split_column9_level1(dfGTF)
+
+    # Level 2 split and collect
+    ulog.print_and_logger_info('Collecting values from key-value pairs...')
+    for k in tqdm(list_keys):
+        build_key_value_column(dfGTF,k)
+
+    # Remove intermediate Level 1 columns.
+    droplabels = []
+    for col in dfGTF:
+        if isinstance(col, int):
+            droplabels.append(col)
+    dfGTF=dfGTF.drop(columns=droplabels)
+    return dfGTF
+
+def build_Metadata_DataFrame(file_pattern: str, path: str, column_headers: list[str]) ->pd.DataFrame:
+
+    # Builds a DataFrame that translates one of the GenCode metadata files.
+    # The specification of metadata files is at https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_41/_README.TXT
+
+    # path: path to folder containing GTF files.
+    # file_pattern: relevant portion of a filename.
+    # [The GenCode version is part of the file name (e.g., gencode.v41.annotation.gtf), so search on the pattern.]
+
+    # Load the "raw" version of the GTF file into a DataFrame.
+
+    # search on a file pattern.
+    dfGTF = load_GTF_into_DataFrame(file_pattern=file_pattern, path=path)
+   # The GTF file does not have column headers. Add these with values from the specification.
+    dfGTF.columns = column_headers
+
+    return dfGTF
 
 class RawTextArgumentDefaultsHelpFormatter(
     argparse.ArgumentDefaultsHelpFormatter,
@@ -259,11 +320,33 @@ else:
 
 # 1. Annotation file
 dfAnnotation = build_Annotation_DataFrame(path=args.owlnets_dir)
+outfile_ann = os.path.join(args.owlnets_dir,'TranslatedAnnotationGTF.csv')
+outfile_entrez = os.path.join(args.owlnets_dir,'EntrezGene.csv')
 
-# TO DO
+# Metadata
 # 2. Entrez file
+dfEntrez = build_Metadata_DataFrame(file_pattern='EntrezGene',path=args.owlnets_dir,column_headers=['transcript_id','Entrez_Gene_id'])
+#dfEntrez.to_csv(outfile_entrez,index=False)
 
-# Join Entrez file to Annotation file.
+# 3. RefSeq file
+dfReqSeq = build_Metadata_DataFrame(file_pattern='RefSeq',path=args.owlnets_dir,column_headers=['transcript_id','RefSeq_RNA_id','RefSeq_protein_id'])
+
+# 4. SwissProt file
+dfSwissProt = build_Metadata_DataFrame(file_pattern='SwissProt',path=args.owlnets_dir,column_headers=['transcript_id','UNIPROTKB_SwissProt_AN','UNIPROTKB_SwissProt_AN2'])
+
+# 4. TrEMBL file
+dfTrEMBL = build_Metadata_DataFrame(file_pattern='TrEMBL',path=args.owlnets_dir,column_headers=['transcript_id','UNIPROTKB_TrEMBL_AN','UNIPROTKB_TrEMBL_AN2'])
+
+
+# Join Metadata files to Annotation file.
+ulog.print_and_logger_info('Merging annotation and metadata.')
+dfAnnotation = dfAnnotation.merge(dfEntrez,how='left',on='transcript_id')
+dfAnnotation = dfAnnotation.merge(dfReqSeq,how='left',on='transcript_id')
+dfAnnotation = dfAnnotation.merge(dfSwissProt,how='left',on='transcript_id')
+dfAnnotation = dfAnnotation.merge(dfTrEMBL,how='left',on='transcript_id')
+
+ulog.print_and_logger_info(f'Writing to {outfile_ann}')
+dfAnnotation.to_csv(outfile_ann,index=False)
 
 # Generate edge file.
 # Generate node file.
