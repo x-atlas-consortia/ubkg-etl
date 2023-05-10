9a70088205fa8c987d28185539c14bdf09443c32
AlanSimmons <jas971@pitt.edu>
2023-05-10 10:03:34 -0500

    Change to codeReplacements functionality, including use of prefixes.csv file for Turtle files.

--- generation_framework/ubkg_utilities/ubkg_parsetools.py
+++ generation_framework/ubkg_utilities/ubkg_parsetools.py
@@ -25,7 +25,6 @@ def codeReplacements(x:pd.Series, ingestSAB: str):
     # This function converts strings that correspond to either codes or CUIs for concepts to a format
     # recognized by the knowledge graph.
 
-
     # Arguments:
     #  x - Pandas Series object containing information on either:
     #      a node (subject or object)
@@ -53,23 +52,24 @@ def codeReplacements(x:pd.Series, ingestSAB: str):
     # For various reasons, some SABs in the UMLS diverge from the format.
     # A common divergence is the case in which the SAB is included in the code to account for codes with leading zeroes
     #-- e.g., HGNC, GO, HPO
-    # NCI
+    # NCI Thesaurus
     ret = ret.str.replace('NCIT ', 'NCI ', regex=False)
-    # MSH
+    # MESH
     ret = ret.str.replace('MESH ', 'MSH ', regex=False)
     # GO
     ret = ret.str.replace('GO ', 'GO GO:', regex=False)
-    # NCBI
+    # NCBI Taxonomy
     ret = ret.str.replace('NCBITaxon ', 'NCBI ', regex=False)
     # UMLS
     ret = ret.str.replace('.*UMLS.*\s', 'UMLS ', regex=True)
     # SNOMED
     ret = ret.str.replace('.*SNOMED.*\s', 'SNOMEDCT_US ', regex=True)
-    # HP
+    # HPO
     ret = ret.str.replace('HP ', 'HPO HP:', regex=False)
     # FMA
     ret = ret.str.replace('^fma', 'FMA ', regex=True)
     # HGNC
+    # Note that non-UMLS sets of assertions may also refer to HGNC codes differently. See below.
     ret = ret.str.replace('Hugo.owl HGNC ', 'HGNC ', regex=False)
     ret = ret.str.replace('HGNC ', 'HGNC HGNC:', regex=False)
     ret = ret.str.replace('gene symbol report?hgnc id=', 'HGNC HGNC:', regex=False)
@@ -77,11 +77,17 @@ def codeReplacements(x:pd.Series, ingestSAB: str):
     # -------------
     # SPECIAL CASES - Non-UMLS sets of assertions
 
+    # Ontologies such as HRAVS refer to NCI Thesaurus nodes by IRI.
     ret = np.where(x.str.contains('Thesaurus.owl'), 'NCI ' + x.str.split('#').str[-1], ret)
 
     # UNIPROTKB
-    # The HGNC codes were in the expected format of HGNC HGNC:code. Remove duplications introduced from earlier conversions.
+    # The HGNC codes in the UNIPROTKB ingest files were in the expected format of HGNC HGNC:code.
+    # Remove duplications introduced from earlier conversions in this script.
     ret = np.where(x.str.contains('HGNC HGNC:'), x, ret)
+
+    # EDAM
+    # EDAM uses subdomains--e.g, format_3750, which translates to a SAB of "format". Force all
+    # EDAM nodes to be in a SAB named EDAM.
 
     # EDAM has two cases:
     # 1. When obtained from edge file for source or object nodes, EDAM IRIs are in the format
@@ -96,7 +102,7 @@ def codeReplacements(x:pd.Series, ingestSAB: str):
     ret = np.where((x.str.contains('edam')), 'EDAM ' + x.str.replace(' ', '_').str.split('/').str[-1], ret)
 
     # MONDO
-    #
+    # Two cases to handle:
     # 1. MONDO identifies genes with IRIs in format
     # http://identifiers.org/hgnc/<id>
     # Convert to HGNC HGNC:<id>
@@ -107,34 +113,40 @@ def codeReplacements(x:pd.Series, ingestSAB: str):
     ret = np.where(x.str.contains('http://purl.obolibrary.org/obo/mondo#'),
                    'MONDO' + x.str.split('#').str[-1], ret)
 
+    # PREFIXES
     # A number of ontologies, especially those that originate from Turtle files, use prefixes that are
     # translated to IRIs that are not formatted as expected. Obtain the original namespace prefixes for
     # SABs.
     # Read in file of prefix mappings.
     dfPrefix = getPrefixes()
+    # Convert for each of the prefixes.
     for index, row in dfPrefix.iterrows():
         if ingestSAB in ['GLYCOCOO','GLYCORDF']:
+            #GlyCoCOO (a Turtle) and GlyCoRDF use IRIs that delimit with has and use underlines.
+            #"http://purl.glycoinfo.org/ontology/codao#Compound_disease_association
             ret = np.where(x.str.contains(row['prefix']), row['SAB'] + ' ' +
                            x.str.replace(' ', '_').str.replace('/', '_').str.split('#').str[-1],
                            ret)
         else:
+            # Other SABs format IRIs with a terminal backslash and the code string.
+            # A notable exception is the PantherDB format (in NPOSKCAN), for which the IRI is an API call
+            # (e.g., http://www.pantherdb.org/panther/family.do?clsAccession=PTHR10558).
             ret = np.where(x.str.contains(row['prefix']),row['SAB']+' '+x.str.replace(' ', '_').str.replace('/','_').str.replace('=','_').str.split('_').str[-1], ret)
 
     # UNIPROT (not to be confused with UNIPROTKB).
+    # UNIPROT IRIs are formatted differently than those in Glygen, but are in the Glygen OWL files, so they need
+    # to be translated separately from GlyGen nodes.
     ret = np.where(x.str.contains('uniprot.org'), 'UNIPROT ' + x.str.split('/').str[-1], ret)
 
     # HRAVS
-    ret = np.where(x.str.contains('http://purl.humanatlas.io/valueset/'), 'HRAVS ' + x.str.split('/').str[-1], ret)
-
-    # GTEX, which formats the SAB as GTEX_(subcategory) (code).
-    ret = np.where(x.str.contains('GTEX_'),x.str.split(' ').str[0]+' '+ x.str.split(' ').str[-1],ret)
+    #ret = np.where(x.str.contains('http://purl.humanatlas.io/valueset/'), 'HRAVS ' + x.str.split('/').str[-1], ret)
 
     # ---------------
     # FINAL PROCESSING
     # JAS 12 JAN 2023 - Force SAB to uppercase.
     # Some CodeIds will be in format SAB <space> <other string>, and <other string> can be mixed case.
     # <other string> can also have spaces.
-    # After all of the preceding conversions, ret is now a numpy array.
+    # After the preceding conversions, ret has changed from a Pandas Series to a numpy array.
     # Split each element; convert the SAB portion to uppercase; and rejoin.
     for idx, x in np.ndenumerate(ret):
         x2 = x.split(sep=' ', maxsplit=1)
